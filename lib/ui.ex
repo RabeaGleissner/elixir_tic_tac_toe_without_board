f80eec46410d06b401f3ee8038f6bedc80f08a7f
Rabea Gleissner <rabeagleissner@gmail.com>
2016-04-28 17:57:28 +0100

    prints error message for invalid input

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,18 +1,39 @@
 defmodule Ui do
 
   def ask_for_position do
-    IO.gets "Please enter a number: "
+    IO.puts "Please enter a number: "
+    ask_user_for_position
+  end
+
+  defp ask_user_for_position do
+    user_input = IO.gets ""
+    input = String.strip(user_input)
+
+    if valid?(input) do
+      convert_to_integer(input)
+    else
+      invalid_position_error
+      ask_for_position
+    end
+  end
+
+  defp invalid_position_error do
+    IO.puts "Sorry, only numbers from 1 - 9 are valid."
   end
 
   def valid?(input) do
     valid_numbers = [1,2,3,4,5,6,7,8,9]
     if is_number?(input) do
       Enum.find(valid_numbers, false, fn(num) ->
-        num == String.to_integer(input)
+        num == convert_to_integer(input)
       end)
     else
       false
     end
+  end
+
+  def convert_to_integer(string) do
+    String.to_integer(string)
   end
 
   defp is_number?(input) do
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -3,14 +3,16 @@ defmodule UiTest do
   import ExUnit.CaptureIO
 
   test "asks user for a position" do
-    assert "Please enter a number: " == capture_io(fn -> Ui.ask_for_position end)
+    assert capture_io([input: "4"], fn ->
+      Ui.ask_for_position
+    end) == "Please enter a number: \n"
   end
 
   test "returns user input for a position" do
     capture_io([input: "4"], fn ->
       send self(), {:result, Ui.ask_for_position}
     end)
-    assert_received {:result, "4"}
+    assert_received {:result, 4}
   end
 
   test "returns false when user input is invalid" do
@@ -19,5 +21,11 @@ defmodule UiTest do
 
   test "returns user input if it is valid" do
     assert 3 == Ui.valid?("3")
+  end
+
+  test "prints an error message and asks again on bad input for position" do
+    assert capture_io([input: "n\n4"], fn ->
+      Ui.ask_for_position
+    end) == "Please enter a number: \nSorry, only numbers from 1 - 9 are valid.\nPlease enter a number: \n"
   end
 end
