5ccf80936782f5e9e636008b7e70307f8953fb28
Rabea Gleissner <rabeagleissner@gmail.com>
2016-04-28 16:08:00 +0100

    returns user input for position

--- lib/ui.ex
+++ lib/ui.ex
@@ -1,7 +1,7 @@
 defmodule Ui do
 
   def ask_for_position do
-    IO.puts "Please enter a number:"
+    IO.gets "Please enter a number: "
   end
 end
 
--- test/ui_test.exs
+++ test/ui_test.exs
@@ -3,6 +3,13 @@ defmodule UiTest do
   import ExUnit.CaptureIO
 
   test "asks user for a position" do
-    assert "Please enter a number:\n" == capture_io(fn -> Ui.ask_for_position end)
+    assert "Please enter a number: " == capture_io(fn -> Ui.ask_for_position end)
+  end
+
+  test "returns user input for a position" do
+    capture_io([input: "4"], fn ->
+      send self(), {:result, Ui.ask_for_position}
+    end)
+    assert_received {:result, "4"}
   end
 end
